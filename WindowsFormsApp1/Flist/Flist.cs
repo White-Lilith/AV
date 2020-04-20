90fb0be581828e98a0e579f8cd01431d6f3833db
White-Lilith <57572200+White-Lilith@users.noreply.github.com>
2020-04-21 01:20:40 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Flist/Flist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flist
+{
+    [Serializable]
+    public class ForList
+    {
+        public string sign { get; set; }
+        public string brand { get; set; }
+        public string color { get; set; }
+        public string name { get; set; }
+        public string n1 { get; set; }
+        public string n2 { get; set; }
+        public decimal year { get; set; }
+    }
+}
